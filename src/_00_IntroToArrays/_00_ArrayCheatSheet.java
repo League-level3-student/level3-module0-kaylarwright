e68491e6751c04d40397f84f350c7f6c2d950650
ilabstaff <ilabstaff@172.16.116.73>
2019-08-20 17:57:29 -0700

    array

--- src/_00_IntroToArrays/_00_ArrayCheatSheet.java
+++ src/_00_IntroToArrays/_00_ArrayCheatSheet.java
@@ -1,19 +1,23 @@
 package _00_IntroToArrays;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
+String[] five = {"Kayla","Kevin","James","Margarett","Catherine"};
 
 		//2. print the third element in the array
-
+System.out.println(five[2]);
 		//3. set the third element to a different value
-
+five[2] = "Jam";
 		//4. print the third element again
-		
+		System.out.println(five[2]);
 		//5. use a for loop to set all the elements in the array to a string of your choice
-		
+		for (int i = 0; i < five.length; i++) {
+		five[i] = "Wright";
+		}
 		//6. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		
